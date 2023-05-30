b0329147f3bf4d31b826d42011866623eb27fc11
Juan Miguel Fernandez <82823629+9ero@users.noreply.github.com>
2023-05-30 11:19:56 -0600

    Adding Eslint and Prettier
    Adding Lint and formatter to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-//import  ReactDOM  from "react-dom";
-import { createRoot } from 'react-dom/client';
+// import  ReactDOM  from "react-dom";
+import { createRoot } from 'react-dom/client'
 
-import "./index.css";
-const app = <h1>Desarrollo util</h1>;
-//const container = document.getElementById("root");
-//ReactDOM.render(app, container);
+import './index.css'
+const app = <h1> Desarrollo util</h1>
+// const container = document.getElementById("root");
+// ReactDOM.render(app, container);
 
-const container1 = document.getElementById('root');
-const root = createRoot(container1); // createRoot(container!) if you use TypeScript
-root.render(app);
+const container1 = document.getElementById('root')
+const root = createRoot(container1) // createRoot(container!) if you use TypeScript
+root.render(app)
